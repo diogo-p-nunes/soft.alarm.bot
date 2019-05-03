ac28892ac7b86b185cf2e61231e1a4c27f5d6dad
diogo-p-nunes <diogo_p_nunes@hotmail.com>
2019-05-03 09:44:36 +0100

    dummy google account tokens

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main {
         try {
             user = new UserInfo();
 
-            // get the start time of the first event of tomorrow
+            // get the start time and location of the first event of tomorrow
             System.out.println(user.getFirstEventStart("2019-05-02T00:00:00Z"));
             System.out.println(user.getFirstEventLocation("2019-05-02T00:00:00Z"));
         }
